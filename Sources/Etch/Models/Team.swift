0b826245ed7fe73310b24b3316177a539a2ef5fa
Jordan Kay <jordan@fleuronic.com>
2023-11-02 15:30:08 -0400

    Add team issue count

--- Sources/Etch/Models/Team.swift
+++ Sources/Etch/Models/Team.swift
@@ -4,12 +4,14 @@ public struct Team {
 	let name: String
 	let creationDate: Date
 	let updateDate: Date
+	let issueCount: Int
 	let isPrivate: Bool
 	
 	public init(
 		name: String,
 		creationDate: Date,
 		updateDate: Date,
+		issueCount: Int,
 		isPrivate: Bool
 	) {
 		self.name = name
