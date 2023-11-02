c4485b519af3a3e0645781277bde4661149a4f83
Jordan Kay <jordan@fleuronic.com>
2023-11-02 13:13:28 -0400

    Add more Issue properties

--- Sources/Etch/Models/Issue.swift
+++ Sources/Etch/Models/Issue.swift
@@ -1,9 +1,20 @@
+import struct Foundation.Date
+
 public struct Issue {
 	let title: String
+	let description: String
+	let creationDate: Date
+	let archiveDate: Date
 	
 	public init(
-		title: String
+		title: String,
+		description: String,
+		creationDate: Date,
+		archiveDate: Date
 	) {
 		self.title = title
+		self.description = description
+		self.creationDate = creationDate
+		self.archiveDate = archiveDate
 	}
 }
