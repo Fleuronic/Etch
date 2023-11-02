d8b5879bd8639f82020cab39786dbdc3ae74e1ed
Jordan Kay <jordan@fleuronic.com>
2023-11-01 22:30:19 -0400

    Add Swift package

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+	name: "Etch",
+	platforms: [
+		.iOS(.v13),
+		.macOS(.v10_15),
+		.tvOS(.v13),
+		.watchOS(.v6)
+	],
+	products: [
+		.library(
+			name: "Etch",
+			targets: [
+				"Etch"
+			]
+		)
+	],
+	dependencies: [],
+	targets: [
+		.target(
+			name: "Etch",
+			dependencies: []
+		)
+	]
+)
