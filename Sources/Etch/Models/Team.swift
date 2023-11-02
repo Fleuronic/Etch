9313ebfae4b90b9aa4b54ee8cd3d9ad320f9ff10
Jordan Kay <jordan@fleuronic.com>
2023-11-02 13:35:12 -0400

    Add team update date

--- Sources/Etch/Models/Team.swift
+++ Sources/Etch/Models/Team.swift
@@ -3,12 +3,15 @@ import struct Foundation.Date
 public struct Team {
 	let name: String
 	let creationDate: Date
+	let updateDate: Date
 	
 	public init(
 		name: String,
-		creationDate: Date
+		creationDate: Date,
+		updateDate: Date
 	) {
 		self.name = name
 		self.creationDate = creationDate
+		self.updateDate = updateDate
 	}
 }
