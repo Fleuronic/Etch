d9108e45613480fa73fb617d68024f9ef2d75ffa
Jordan Kay <jordan@fleuronic.com>
2023-11-02 15:03:06 -0400

    Add team private property

--- Sources/Etch/Models/Team.swift
+++ Sources/Etch/Models/Team.swift
@@ -4,14 +4,17 @@ public struct Team {
 	let name: String
 	let creationDate: Date
 	let updateDate: Date
+	let isPrivate: Bool
 	
 	public init(
 		name: String,
 		creationDate: Date,
-		updateDate: Date
+		updateDate: Date,
+		isPrivate: Bool
 	) {
 		self.name = name
 		self.creationDate = creationDate
 		self.updateDate = updateDate
+		self.isPrivate = isPrivate
 	}
 }
