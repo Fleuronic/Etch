f2685d55ae4471dc63f8c5d8a8ae5672d65c4a77
Jordan Kay <jordan@fleuronic.com>
2023-11-02 21:18:11 -0400

    Support init macro

--- Sources/Etch/Models/Team.swift
+++ Sources/Etch/Models/Team.swift
@@ -1,23 +1,11 @@
+import InitMacro
 import struct Foundation.Date
 
-public struct Team {
+@Init public struct Team {
 	let name: String
 	let creationDate: Date
 	let updateDate: Date
+	let archiveDate: Date
 	let issueCount: Int
 	let isPrivate: Bool
-	
-	public init(
-		name: String,
-		creationDate: Date,
-		updateDate: Date,
-		issueCount: Int,
-		isPrivate: Bool
-	) {
-		self.name = name
-		self.creationDate = creationDate
-		self.updateDate = updateDate
-		self.issueCount = issueCount
-		self.isPrivate = isPrivate
-	}
 }
