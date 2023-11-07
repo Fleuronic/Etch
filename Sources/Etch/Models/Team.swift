5c65aad4170621b1aba6a98430b2f3d9ddf24641
Jordan Kay <jordan@fleuronic.com>
2023-11-06 19:26:24 -0500

    Temporarily remove most properties from team

--- Sources/Etch/Models/Team.swift
+++ Sources/Etch/Models/Team.swift
@@ -5,37 +5,38 @@ import struct Foundation.TimeZone
 import struct Foundation.TimeInterval
 
 @Init public struct Team {
-	public let key: String
+	// TODO: Restore Properties
+	// public let key: String
 	public let name: String
-	public let icon: String?
-	public let color: String?
-	public let description: String?
-	public let creationDate: Date
-	public let updateDate: Date
-	public let archiveDate: Date?
-	public let timeZone: TimeZone
-	public let groupsIssueHistory: Bool
-	public let defaultsIssueSortOrderToBottom: Bool
-	public let allowsZeroIssueEstimation: Bool
-	public let isIssueEstimationExtended: Bool
-	public let isIssueOrderingNoPriorityFirst: Bool
-	public let isPrivate: Bool
-	public let isTriageEnabled: Bool
-	public let requiresPriorityToLeaveTriage: Bool
-	public let inviteHash: String
-	public let autoArchivePeriod: TimeInterval
-	public let autoClosePeriod: TimeInterval?
-	public let defaultIssueEstimate: Int
-	public let areCyclesEnabled: Bool
-	public let cycleStartDay: Int
-	public let cycleDuration: TimeInterval
-	public let cycleCooldownTime: TimeInterval
-	public let cycleCalendarUrl: URL
-	public let upcomingCycleCount: Int
-	public let isCycleAutoAssignedActiveIssues: Bool
-	public let isCycleAutoAssignedStartedIssues: Bool
-	public let isCycleAutoAssignedCompletedIssues: Bool
-	public let sendsSlackIssueComments: Bool
-	public let sendsSlackIssueStatuses: Bool
-	public let sendsSlackNewIssue: Bool
+	// public let icon: String?
+	// public let color: String?
+	// public let description: String?
+	// public let creationDate: Date
+	// public let updateDate: Date
+	// public let archiveDate: Date?
+	// public let timeZone: TimeZone
+	// public let groupsIssueHistory: Bool
+	// public let defaultsIssueSortOrderToBottom: Bool
+	// public let allowsZeroIssueEstimation: Bool
+	// public let isIssueEstimationExtended: Bool
+	// public let isIssueOrderingNoPriorityFirst: Bool
+	// public let isPrivate: Bool
+	// public let isTriageEnabled: Bool
+	// public let requiresPriorityToLeaveTriage: Bool
+	// public let inviteHash: String
+	// public let autoArchivePeriod: TimeInterval
+	// public let autoClosePeriod: TimeInterval?
+	// public let defaultIssueEstimate: Int
+	// public let areCyclesEnabled: Bool
+	// public let cycleStartDay: Int
+	// public let cycleDuration: TimeInterval
+	// public let cycleCooldownTime: TimeInterval
+	// public let cycleCalendarUrl: URL
+	// public let upcomingCycleCount: Int
+	// public let isCycleAutoAssignedActiveIssues: Bool
+	// public let isCycleAutoAssignedStartedIssues: Bool
+	// public let isCycleAutoAssignedCompletedIssues: Bool
+	// public let sendsSlackIssueComments: Bool
+	// public let sendsSlackIssueStatuses: Bool
+	// public let sendsSlackNewIssue: Bool
 }
